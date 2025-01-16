51a60393ef39e8c2d40f6fcf50ac670096bd40ec
Urban Müller <152815434+Jadycarai@users.noreply.github.com>
2025-01-16 17:30:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-document.querySelector('.follow-btn').addEventListener('click', function() {
-    alert('Você começou a seguir @simo!');
-});
 
-document.querySelector('.message-btn').addEventListener('click', function() {
-    alert('Função de mensagem será implementada!');
-});
