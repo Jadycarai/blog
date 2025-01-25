3e08ddd8d885a367973a1c2aa18ff319c3f22142
Urban Müller <152815434+Jadycarai@users.noreply.github.com>
2025-01-25 15:00:37 -0300

    Update memoria.js

--- memoria.js
+++ memoria.js
@@ -1,9 +1,1 @@
-document.querySelectorAll('.fragment').forEach(fragment => {
-    fragment.addEventListener('mouseover', () => {
-        fragment.style.transform = 'scale(1.05)';
-    });
 
-    fragment.addEventListener('mouseout', () => {
-        fragment.style.transform = 'scale(1)';
-    });
-});
