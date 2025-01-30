ba490f7c4897b1e6c30215a136ba89c7516a81d1
Urban Müller <152815434+Jadycarai@users.noreply.github.com>
2025-01-30 17:20:49 -0300

    Update memoria.js

--- memoria.js
+++ memoria.js
@@ -1,1 +1,12 @@
+const image = document.querySelector('.glitch-image');
+const sound = document.getElementById('glitch-sound');
 
+image.addEventListener('mouseover', () => {
+    image.style.transform = 'scale(1.1)';
+    sound.play();
+});
+
+image.addEventListener('mouseout', () => {
+    image.style.transform = 'scale(1)';
+    sound.pause();
+});
